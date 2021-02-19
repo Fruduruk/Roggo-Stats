65ad0318ac9eb1e91423e2e2db9c406e0ad5dc8d
Fruduruk <fruduruk@users.noreply.github.com>
2021-02-19 15:36:44 +0100

    Refactored DataPackMerger.cs

--- RLStats Classes/MainClasses/DataPackMerger.cs
+++ RLStats Classes/MainClasses/DataPackMerger.cs
@@ -6,56 +6,9 @@ namespace RLStats_Classes.MainClasses
 {
     public class DataPackMerger
     {
-        private const string NoDataPackToMerge = "no datapack to merge";
-        public List<ApiDataPack> DataPacks = new List<ApiDataPack>();
-        private List<Replay> commonReplays;
-
-        public ApiDataPack GetDataMerge()
-        {
-            if (DataPacks.Count == 1)
-                return DataPacks[0];
-            if (DataPacks.Count == 0)
-                return new ApiDataPack()
-                {
-                    Success = false,
-                    ReplayCount = 0,
-                    Replays = new List<Replay>(),
-                    Ex = new Exception(NoDataPackToMerge),
-                    ReceivedString = NoDataPackToMerge
-                };
-            foreach (ApiDataPack dataPack in DataPacks)
-                if (!dataPack.Success)
-                    return dataPack;
-
-            commonReplays = new List<Replay>();
-            for (int j = 0; j < DataPacks[0].Replays.Count; j++)
-            {
-                Replay r = DataPacks[0].Replays[j];
-                if (IfEverybodyHas(r))
-                    commonReplays.Add(r);
-            }
-            return new ApiDataPack()
-            {
-                Success = true,
-                Replays = commonReplays
-            };
-
-        }
-
-        private bool IfEverybodyHas(Replay r)
-        {
-            for (int i = 1; i < DataPacks.Count; i++)
-                if (!DoesListContainReplay(DataPacks[i].Replays, r))
-                {
-                    return false;
-                }
-
-            return true;
-        }
-
         public static List<Replay> GetReplaysForTimeZone(List<Replay> replays, DateTime start, DateTime end)
         {
-            List<Replay> replaysInTimeZone = new List<Replay>();
+            var replaysInTimeZone = new List<Replay>();
             foreach(var r in replays)
             {
                 if (r.Date > start)
