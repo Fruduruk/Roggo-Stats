c3ad7f9ae82071b372e1b0310f8084fd25a1c14b
Fruduruk <fruduruk@users.noreply.github.com>
2021-02-19 14:45:48 +0100

    Renamed Mvp in AveragePlayerCore.cs

--- RLStats Classes/AverageModels/AveragePlayerCore.cs
+++ RLStats Classes/AverageModels/AveragePlayerCore.cs
@@ -2,7 +2,7 @@
 {
     public class AveragePlayerCore
     {
-        public double? MVP { get; set; }
+        public double? Mvp { get; set; }
         public double? Shots { get; set; }
         public double? Shots_against { get; set; }
         public double? Goals { get; set; }
