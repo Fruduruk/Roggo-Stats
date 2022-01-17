e4a0d5c85055c12491dcd721233dd1ec767b02fc
Fruduruk <fruduruk@users.noreply.github.com>
2022-01-17 09:58:31 +0100

    Added Discord Bot

---
+++ RLStats.DiscordBot/Modules/RLStats/RlStatsBoostModule.cs
@@ -0,0 +1,45 @@
+﻿using Discord.Commands;
+
+using Microsoft.Extensions.Logging;
+
+using RLStats_Classes.AverageModels;
+
+using System;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Modules.RLStats
+{
+    public class RlStatsBoostModule : RlStatsModuleBase
+    {
+        public RlStatsBoostModule(ILogger<RlStatsModuleBase> logger, string ballchasingToken) : base(logger, ballchasingToken)
+        {
+        }
+
+        [Command("boost compare")]
+        public async Task Compare(string time, string together, params string[] names)
+        {
+            await Compare<AveragePlayerBoost>(time, names, playedTogether: ConvertTogetherToBool(together));
+        }
+
+        [Command("boost today")]
+        public async Task BoostToday(string together, params string[] names)
+        {
+            var averages = await AverageStatsForTime(names, new Tuple<DateTime, DateTime>(DateTime.Today, DateTime.Today + new TimeSpan(1, 0, 0, 0)), playedTogether: ConvertTogetherToBool(together));
+            await OutputEpicAsync<AveragePlayerBoost>(averages);
+        }
+
+        [Command("boost all")]
+        public async Task BoostAllTime(string together, params string[] names)
+        {
+            var averages = await AverageStatsForTime(names, playedTogether: ConvertTogetherToBool(together));
+            await OutputEpicAsync<AveragePlayerBoost>(averages);
+        }
+
+        [Command("boost last")]
+        public async Task BoostLast(int count, string together, params string[] names)
+        {
+            var averages = await AverageStatsForTime(names, replayCap: count, playedTogether: ConvertTogetherToBool(together));
+            await OutputEpicAsync<AveragePlayerBoost>(averages);
+        }
+    }
+}
---
+++ RLStats.DiscordBot/RLStatsHostBuilder.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Discord_Bot.Services;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System.IO;
+
+namespace Discord_Bot
+{
+    public class RLStatsHostBuilder
+    {
+        private IHostBuilder _builder;
+
+        public RLStatsHostBuilder()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
+
+                    x.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Debug); // Defines what kind of information should be logged (e.g. Debug, Information, Warning, Critical) adjust this to your liking
+                })
+                .ConfigureDiscordHost((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Verbose, // Defines what kind of information should be logged from the API (e.g. Verbose, Info, Warning, Critical) adjust this to your liking
+                        AlwaysDownloadUsers = true,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Verbose;
+                    config.DefaultRunMode = RunMode.Async;
+                    config.CaseSensitiveCommands = false;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton(context.Configuration["ballchasing-token"].ToString());
+                    services.AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+            _builder = builder;
+        }
+
+        public IHost GetHost()
+        {
+            return _builder.Build();
+        }
+    }
+}
