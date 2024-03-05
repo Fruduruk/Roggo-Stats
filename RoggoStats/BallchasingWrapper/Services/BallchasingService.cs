dae3a9b9351b30d40a8ed80690b22dc12c6a1d88
Fruduruk <fruduruk@users.noreply.github.com>
2024-03-05 16:29:07 +0100

    edited proto

--- RoggoStats/BallchasingWrapper/Services/BallchasingService.cs
+++ RoggoStats/BallchasingWrapper/Services/BallchasingService.cs
@@ -1,4 +1,7 @@
-using BallchasingWrapper.Grpc;
+using BallchasingWrapper.BusinessLogic;
+using BallchasingWrapper.DB.MongoDB;
+using BallchasingWrapper;
+using Google.Protobuf.Collections;
 using Grpc.Core;
 
 namespace BallchasingWrapper.Services;
@@ -6,26 +9,22 @@ namespace BallchasingWrapper.Services;
 public class BallchasingService : Grpc.Ballchasing.BallchasingBase
 {
     private readonly ILogger<BallchasingService> _logger;
+    private readonly ReplayProvider _replayProvider;
+    private readonly AdvancedReplayProvider _advancedReplayProvider;
 
-    public BallchasingService(ILogger<BallchasingService> logger)
+    public BallchasingService(ILogger<BallchasingService> logger, BallchasingApi api, RlStatsMongoDatabase db)
     {
         _logger = logger;
+        _replayProvider = new ReplayProvider(api, db, logger);
+        _advancedReplayProvider = new AdvancedReplayProvider(api, db, logger);
     }
 
-    public override Task<SimpleReplaysResponse> GetSimpleReplays(RequestFilter request, ServerCallContext context)
+    public override async Task<Grpc.SimpleReplaysResponse> GetSimpleReplays(Grpc.RequestFilter request, ServerCallContext context)
     {
-        return Task.FromResult(new SimpleReplaysResponse
+        //var response = await _replayProvider.CollectReplaysAsync(null);
+        return await Task.FromResult(new Grpc.SimpleReplaysResponse
         {
-            Replays = { new Replay
-            {
-                Orange = new Team
-                {
-                    Players = { new Player
-                    {
-                        Score = 0
-                    } }
-                }
-            } }
+            Replays = {  },
         });
     }
 }
--- RoggoStats/RustGRPCClient/src/main.rs
+++ RoggoStats/RustGRPCClient/src/main.rs
@@ -1,25 +1,34 @@
 mod client;
 
-use ballchasing_api::basic_api_client;
-use crate::ballchasing_api::basic_api_client::BasicApiClient;
-use crate::ballchasing_api::SimpleReplayRequest;
+use ballchasing::ballchasing_client;
+use crate::ballchasing::ballchasing_client::BallchasingClient;
+use crate::ballchasing::RequestFilter;
 
-pub mod ballchasing_api{
-    tonic::include_proto!("ballchasing_api");
+pub mod ballchasing{
+    tonic::include_proto!("ballchasing");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("This is my Greetings gRPC test.");
 
-    let mut client = BasicApiClient::connect("http://localhost:5007").await?;
+    let mut client = BallchasingClient::connect("http://localhost:5007").await?;
 
-    let request = tonic::Request::new(SimpleReplayRequest {
-        id: "Fruduruk".into(),
+    let request = tonic::Request::new(RequestFilter {
+        replay_cap: None,
+        filter_name : None,
+        names: vec!["Fruduruk".into()],
+        title: None,
+        playlist: None,
+        free_to_play: None,
+        season: None,
+        steam_ids: vec![],
+        min_date: None,
+        max_date: None,
     });
 
 
-    let response = client.get_simple_replay(request).await?;
+    let response = client.get_simple_replays(request).await?;
 
 
 
