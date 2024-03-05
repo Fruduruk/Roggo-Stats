af5d2833b2e34468c5c2a3c6b89647a2016f86a6
Fruduruk <fruduruk@users.noreply.github.com>
2024-03-05 16:29:07 +0100

    edited proto

---
+++ RoggoStats/BallchasingWrapper/GrpcConvert.cs
@@ -0,0 +1,11 @@
+﻿using BallchasingWrapper.Models.ReplayModels;
+
+namespace BallchasingWrapper;
+
+public static class GrpcConvert
+{
+    public static Grpc.Replay ToGrpcReplay(this Replay replay)
+    {
+        return new Grpc.Replay();
+    }
+}
--- RoggoStats/RustGRPCClient/build.rs
+++ RoggoStats/RustGRPCClient/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Compiling proto files...");
-    tonic_build::compile_protos("proto/basicAPI.proto")?;
+    tonic_build::compile_protos("proto/ballchasing.proto")?;
     Ok(())
 }
