d2f53546fa995fa2777984ae691f8371dfcbd0d7
Fruduruk <fruduruk@users.noreply.github.com>
2024-04-01 16:03:13 +0200

    added Docker support

--- RoggoStats/RoggoDiscordBot/main.py
+++ RoggoStats/RoggoDiscordBot/main.py
@@ -11,6 +11,18 @@ BALLCHASING_PORT = os.getenv('BALLCHASING_PORT')
 bot = discord.Bot()
 
 
+def check_environment_variables() -> bool:
+    if DISCORD_BOT_TOKEN is None:
+        print("Please set DISCORD_BOT_TOKEN")
+    if BALLCHASING_HOST is None:
+        print("Please set BALLCHASING_HOST")
+    if BALLCHASING_PORT is None:
+        print("Please set BALLCHASING_PORT")
+    if any([DISCORD_BOT_TOKEN, BALLCHASING_HOST, BALLCHASING_PORT]) is None:
+        return False
+    return True
+
+
 def create_stub() -> ballchasing_pb2_grpc.BallchasingStub:
     channel = grpc.insecure_channel(BALLCHASING_HOST + ':' + BALLCHASING_PORT)
     return ballchasing_pb2_grpc.BallchasingStub(channel)
@@ -33,11 +45,6 @@ async def get_advanced_replays(stub: ballchasing_pb2_grpc.BallchasingStub,
         print(f"Ein Fehler ist aufgetreten: {e}")
 
 
-def main() -> None:
-    print('Roggo Discord Bot started.')
-    bot.run(DISCORD_BOT_TOKEN)
-
-
 @bot.slash_command()
 async def replays(ctx, name: str = None):
     if not name:
@@ -50,6 +57,14 @@ async def replays(ctx, name: str = None):
     replays = await get_advanced_replays(stub, [identity])
     titles = [replay.title for replay in replays]
     await ctx.respond("\n".join(titles))
+
+
+def main() -> None:
+    print('Roggo Discord Bot started.')
+    if not check_environment_variables():
+        print('Stopping program...')
+    else:
+        bot.run(DISCORD_BOT_TOKEN)
 
 
 if __name__ == '__main__':
