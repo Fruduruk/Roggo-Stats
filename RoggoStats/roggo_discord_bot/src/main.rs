c83de086c17acb15179f2ca391249a3d8e88c1c3
Fruduruk <fruduruk@users.noreply.github.com>
2024-04-02 21:45:22 +0200

    grpc test

--- RoggoStats/roggo_discord_bot/src/main.rs
+++ RoggoStats/roggo_discord_bot/src/main.rs
@@ -1,6 +1,5 @@
-use ballchasing::ballchasing_client;
 use crate::ballchasing::ballchasing_client::BallchasingClient;
-use crate::ballchasing::{GroupType, Identity, IdentityType, MatchType, Playlist, FilterRequest};
+use crate::ballchasing::{FilterRequest, GroupType, Identity, IdentityType, MatchType, Playlist};
 
 pub mod ballchasing {
     tonic::include_proto!("ballchasing");
@@ -8,7 +7,7 @@ pub mod ballchasing {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("This is my Greetings gRPC test.");
+    println!("Started roggo discord bot.");
 
     let mut client = BallchasingClient::connect("http://192.168.178.30:9000").await?;
 
@@ -20,22 +19,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 name_or_id: "Fruduruk".into(),
             }
         ],
-        title: None,
         playlist: Playlist::Doubles.into(),
         match_type: MatchType::Ranked.into(),
+        title: None,
         season: None,
         min_date: None,
         max_date: None,
-        group_type: GroupType::Together.into(),
         min_rank: None,
+        group_type: GroupType::Together.into(),
         max_rank: None,
     });
 
-
     let response = client.get_advanced_replays(request).await?;
-    let players = response;
 
-    println!("{:?}", &players);
+    let replays = response.into_inner().replays;
+    let titles: Vec<String> = replays.into_iter().map(|replay| replay.title).collect();
+
+    println!("{:?}", &titles);
 
     Ok(())
 }
