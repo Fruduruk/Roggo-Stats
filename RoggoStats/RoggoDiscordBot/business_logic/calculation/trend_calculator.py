3826ec7114b0cd541fdaab4287509bcfd5205588
Fruduruk <fruduruk@users.noreply.github.com>
2024-04-05 15:01:10 +0200

    added more stats

--- RoggoStats/RoggoDiscordBot/business_logic/calculation/trend_calculator.py
+++ RoggoStats/RoggoDiscordBot/business_logic/calculation/trend_calculator.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from typing import List, Mapping
+from typing import List
 
 import ballchasing_pb2 as bc
 import matplotlib.pyplot as plt
 import numpy as np
 
 from business_logic.calculation.calc_utils import find_advanced_player_in_advanced_replay
+from business_logic.calculation.value_fetcher import get_value
 from business_logic.grpc.grpc_client import get_advanced_replays
 from business_logic.utils import get_basic_result
 from models.time_series_player_stats import TimeSeriesPlayerStats
@@ -14,7 +15,7 @@ from models.trend_result import TrendResult
 
 
 def generate_image(maps: List[TimeSeriesPlayerStats], statistic: Statistic) -> str:
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(10, 6))
     colors = ["b", "r", "g", "y"]
 
     all_dates = sorted({date for value_map in maps for date in value_map.values.keys()})
@@ -35,6 +36,15 @@ def generate_image(maps: List[TimeSeriesPlayerStats], statistic: Statistic) -> s
             if valid_values:
                 m, b = np.polyfit(nums, valid_values, 1)
                 plt.plot(nums, valid_values, marker="o", linestyle=" ", color=color, label=f"{value_map.name}")
+
+    for value_map, color in zip(maps, colors):
+        if len(value_map.values) > 0:
+            values = [value_map.values.get(date) for date in all_dates]
+
+            nums, valid_values = zip(*[(i, val) for i, val in enumerate(values) if val is not None])
+
+            if valid_values:
+                m, b = np.polyfit(nums, valid_values, 1)
                 plt.plot(nums, m * np.array(nums) + b, linestyle="--", color=color)
 
     plt.xticks(ticks=tick_indices, labels=tick_labels, rotation=45)
@@ -44,18 +54,12 @@ def generate_image(maps: List[TimeSeriesPlayerStats], statistic: Statistic) -> s
     plt.legend()
     plt.tight_layout()
 
-    path = "trend_over_time.png"
+    path_names = "-".join([stat.name for stat in maps])
+    pat_date = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    path = f"trend_over_time_{str(statistic)}_{path_names}_{pat_date}.png"
     plt.savefig(path)
     plt.close()
     return path
-
-
-def get_value(advanced_player: bc.AdvancedPlayer, statistic: Statistic) -> float:
-    match statistic:
-        case Statistic.PERCENT_SUPERSONIC_SPEED:
-            return advanced_player.stats.movement.percentSupersonicSpeed
-        case Statistic.BOOST_COLLECTED_PER_MINUTE:
-            return advanced_player.stats.boost.generalBoost.bcpm
 
 
 async def calculate_trend(request: bc.FilterRequest, statistic: Statistic) -> TrendResult:
