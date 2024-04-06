2f6c8761a1f36c2ba198930c0b16ba8f3649d81b
Fruduruk <fruduruk@users.noreply.github.com>
2024-04-06 11:03:52 +0200

    removed old code

--- RLStats.DiscordBot/Singletons/CommandsToProceed.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Discord_Bot.Singletons
-{
-    public class CommandsToProceed
-    {
-        public List<CommandInProgress> CommandsInProgress { get; set; } = new List<CommandInProgress>();
-    }
-}
