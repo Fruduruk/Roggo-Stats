8bdc673ea5fba31ac71465e941551f401bffa7c7
Fruduruk <fruduruk@users.noreply.github.com>
2024-04-06 13:03:25 +0200

    added statistic

--- RoggoStats/RoggoDiscordBot/models/statistic.py
+++ RoggoStats/RoggoDiscordBot/models/statistic.py
@@ -6,21 +6,22 @@ class Statistic(Enum):
     BOOST_COLLECTED_PER_MINUTE = 1
     BOOST_AMOUNT_STOLEN = 2
     BOOST_AMOUNT_USED_WHILE_SUPERSONIC = 3
-    PERCENT_SLOW_SPEED = 4
-    PERCENT_SUPERSONIC_SPEED = 5
-    AVERAGE_SPEED = 6
-    PERCENT_HIGH_AIR = 7
-    TIME_POWERSLIDE = 8
-    COUNT_POWERSLIDE = 9
-    AVERAGE_DISTANCE_TO_BALL = 10
-    AVERAGE_DISTANCE_TO_MATES = 11
-    PERCENT_CLOSEST_TO_BALL = 12
-    PERCENT_FARTHEST_FROM_BALL = 13
-    PERCENT_MOST_BACK = 14
-    PERCENT_MOST_FORWARD = 15
-    GOALS_AGAINST_WHILE_LAST_DEFENDER = 16
-    DEMOS_INFLICTED = 17
-    DEMOS_TAKEN = 18
+    BOOST_COLLECTED_SMALL_TO_BIG_RATIO = 4
+    PERCENT_SLOW_SPEED = 5
+    PERCENT_SUPERSONIC_SPEED = 6
+    AVERAGE_SPEED = 7
+    PERCENT_HIGH_AIR = 8
+    TIME_POWERSLIDE = 9
+    COUNT_POWERSLIDE = 10
+    AVERAGE_DISTANCE_TO_BALL = 11
+    AVERAGE_DISTANCE_TO_MATES = 12
+    PERCENT_CLOSEST_TO_BALL = 13
+    PERCENT_FARTHEST_FROM_BALL = 14
+    PERCENT_MOST_BACK = 15
+    PERCENT_MOST_FORWARD = 16
+    GOALS_AGAINST_WHILE_LAST_DEFENDER = 17
+    DEMOS_INFLICTED = 18
+    DEMOS_TAKEN = 19
 
     def __str__(self) -> str:
         match self:
@@ -31,7 +32,9 @@ class Statistic(Enum):
             case Statistic.BOOST_AMOUNT_STOLEN:
                 return "boost amount stolen"
             case Statistic.BOOST_AMOUNT_USED_WHILE_SUPERSONIC:
-                return "boost amount used when supersonic"
+                return "boost amount used while supersonic"
+            case Statistic.BOOST_COLLECTED_SMALL_TO_BIG_RATIO:
+                return "boost count collected small to big ratio"
             case Statistic.PERCENT_SLOW_SPEED:
                 return "percent slow speed"
             case Statistic.PERCENT_SUPERSONIC_SPEED:
