fcce54f2211b2ac932d991126d9c7376f3b6447e
Fruduruk <fruduruk@users.noreply.github.com>
2026-03-23 16:32:27 +0100

    trend is dark mode now

--- RoggoStats/RoggoDiscordBot/business_logic/discord_bot.py
+++ RoggoStats/RoggoDiscordBot/business_logic/discord_bot.py
@@ -1,6 +1,6 @@
 import interactions
-from config import APP_VERSION 
-
+from interactions.api.events import Startup
+from config import APP_VERSION
 from business_logic.extensions_loader import load_extensions
 from interactions.models import discord
 
@@ -10,11 +10,9 @@ print("loading extensions...")
 load_extensions(bot)
 
 
-@bot.listen()
+@bot.listen(Startup)
 async def on_startup():
     activity = discord.Activity.create(
-        name="Version => " + APP_VERSION,
-        type=discord.activity.ActivityType.PLAYING
+        name="Version: " + APP_VERSION, type=discord.activity.ActivityType.PLAYING
     )
     await bot.change_presence(status=discord.Status.ONLINE, activity=activity)
-
