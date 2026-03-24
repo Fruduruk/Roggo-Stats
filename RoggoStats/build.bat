95cc38c1d8c9f52a45eef83631fd20119ac7f2ca
Fruduruk <fruduruk@users.noreply.github.com>
2026-03-24 15:54:03 +0100

    Renamed Arguments

--- RoggoStats/build.bat
+++ RoggoStats/build.bat
@@ -1,2 +1,3 @@
 docker build --build-arg APP_VERSION=dev -t ballchasing-wrapper:dev ./BallchasingWrapper/.
 docker build --build-arg APP_VERSION=dev -t roggo-discord-bot:dev ./RoggoDiscordBot/.
+docker image prune -f
