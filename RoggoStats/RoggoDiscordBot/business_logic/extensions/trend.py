d18b0be84f0ad5a96503b6dd62c567b85ca97df0
Fruduruk <fruduruk@users.noreply.github.com>
2026-03-25 13:56:22 +0100

    add new parser to every command

--- RoggoStats/RoggoDiscordBot/business_logic/extensions/trend.py
+++ RoggoStats/RoggoDiscordBot/business_logic/extensions/trend.py
@@ -13,6 +13,7 @@ from interactions import (
 )
 
 
+from business_logic.parser import parse_names
 from business_logic.slash_command_choices import GROUP_TYPE_CHOICES, MATCH_TYPE_CHOICES, PLAYLIST_CHOICES, STATISTIC_CHOICES, TIME_CHOICES
 from business_logic.grpc.grpc_helper_functions import to_identity
 from models.statistic import Statistic
@@ -32,7 +33,7 @@ class Trend(Extension):
     )
     @slash_option(
         name="names",
-        description="Trage Namen getrennt mit Komma ein",
+        description="Trage Namen oder @user getrennt mit Komma ein",
         required=True,
         opt_type=OptionType.STRING,
     )
@@ -74,6 +75,12 @@ class Trend(Extension):
         match_type: int = None,
         group_type: int = 0,
     ):
+        identities, unknown_identities = parse_names(names)
+
+        if unknown_identities:
+            await ctx.send(f"Users not registered: {unknown_identities}", ephemeral=True)
+            return
+        
         print(
             f"calculating trend for {time_range},{names},{playlist},{match_type},{statistic}..."
         )
--- RoggoStats/RoggoDiscordBot/business_logic/parser.py
+++ RoggoStats/RoggoDiscordBot/business_logic/parser.py
@@ -1,8 +1,39 @@
 import re
 
+from business_logic.grpc.grpc_helper_functions import to_identity
+from db.storage import try_load_steam_id
+import ballchasing_pb2 as bc
+
 
 def try_parse_discord_id(discord_id: str) -> int | None:
     match = re.fullmatch(r"<@!?(\d+)>", discord_id.strip())
     if match is None:
         return None
     return int(match.group(1))
+
+
+def parse_names(names: str) -> tuple[list[bc.Identity], list[str]]:
+    strings = [
+        name.strip() for name in names.split(",")
+    ]
+    identities = []
+    unknown_identities = []
+    for name in strings:
+        if "@" not in name:
+            identities.append(to_identity(name))
+            continue
+
+        discord_id = try_parse_discord_id(name)
+        if discord_id is None:
+            unknown_identities.append(name)
+            continue
+
+        steam_id = try_load_steam_id(discord_id)
+        if steam_id is None:
+            unknown_identities.append(name)
+            continue
+
+        identities.append(bc.Identity(
+            identityType=bc.STEAM_ID, nameOrId=str(steam_id)))
+        
+    return identities, unknown_identities
