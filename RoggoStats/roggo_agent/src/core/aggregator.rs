3a9c0e8a74a857a78db361f9c0f882337d8fad83
Fruduruk <fruduruk@users.noreply.github.com>
2026-05-03 23:04:37 +0200

    Add Event enum

--- RoggoStats/roggo_agent/src/core/aggregator.rs
+++ RoggoStats/roggo_agent/src/core/aggregator.rs
@@ -3,10 +3,10 @@ use std::collections::HashMap;
 use serde::Deserialize;
 use serde_json::Deserializer;
 
-use crate::core::models::{BallHit, Event, Packet};
+use crate::core::models::{BallHit, Event, RawEvent, RawPacket};
 pub struct Aggregator {
     count: u64,
-    history: HashMap<Event, u32>,
+    history: HashMap<RawEvent, u32>,
 }
 
 impl Aggregator {
@@ -18,10 +18,11 @@ impl Aggregator {
     }
 
     pub fn next(&mut self, raw: String) {
-        let stream = serde_json::Deserializer::from_str(&raw).into_iter::<Packet>();
+        let stream = serde_json::Deserializer::from_str(&raw).into_iter::<RawPacket>();
 
         for json in stream {
             if let Ok(raw) = json {
+                *self.history.entry(raw.event).or_insert(0) += 1;
                 self.next_packet(raw);
             } else {
                 println!("Error while parsing next tcp packet");
@@ -29,23 +30,19 @@ impl Aggregator {
         }
     }
 
-    pub fn next_packet(&mut self, packet: Packet) {
-        let event = packet.event;
-        // println!("Parsed Event {}: {event:?}", self.count);
-
-        *self.history.entry(event).or_insert(0) += 1;
-
-        // println!("History:");
-        for (event, count) in &self.history {
-            // println!("{event:?}:{count}");
-        }
-        if packet.event == Event::BallHit {
-            let result = serde_json::from_str::<BallHit>(&packet.data);
-            if let Ok(data) = result {
-                println!("{data:?}");
+    pub fn next_packet(&mut self, packet: RawPacket) {
+        let event = match Event::new(packet) {
+            Ok(event) => event,
+            Err(err) => {
+                println!("Error while parsing raw event: {}", err);
+                return;
             }
-        }
+        };
+        println!("Parsed Event {}: {event:#?}", self.count);
 
+        // if let Event::BallHit(data) = event {
+        //     println!("{data:#?}");
+        // }
         self.count += 1;
     }
 }
