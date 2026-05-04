4c33dd776dfadbeb7b8c264570c14c2033af9b31
Fruduruk <fruduruk@users.noreply.github.com>
2026-05-04 17:47:25 +0200

    fix debugger and add guid deserializer

--- RoggoStats/roggo_agent/src/core/aggregator.rs
+++ RoggoStats/roggo_agent/src/core/aggregator.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-use serde::Deserialize;
-use serde_json::Deserializer;
 
 use crate::core::models::{BallHit, Event, RawEvent, RawPacket};
 pub struct Aggregator {
@@ -21,28 +19,30 @@ impl Aggregator {
         let stream = serde_json::Deserializer::from_str(&raw).into_iter::<RawPacket>();
 
         for json in stream {
-            if let Ok(raw) = json {
-                *self.history.entry(raw.event).or_insert(0) += 1;
-                self.next_packet(raw);
-            } else {
-                println!("Error while parsing next tcp packet");
+            match json {
+                Ok(raw) => {
+                    *self.history.entry(raw.event).or_insert(0) += 1;
+                    self.next_packet(raw);
+                }
+                Err(err) => {
+                    println!("Error while parsing next tcp packet {err}");
+                }
             }
         }
     }
 
     pub fn next_packet(&mut self, packet: RawPacket) {
+        let save = packet.clone();
         let event = match Event::new(packet) {
             Ok(event) => event,
             Err(err) => {
                 println!("Error while parsing raw event: {}", err);
+                println!("Could not parse this packet: {save:#?}");
                 return;
             }
         };
         println!("Parsed Event {}: {event:#?}", self.count);
 
-        // if let Event::BallHit(data) = event {
-        //     println!("{data:#?}");
-        // }
         self.count += 1;
     }
 }
