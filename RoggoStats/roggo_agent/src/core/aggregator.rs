690bc2aadc4d15b43797f4d8dd07f7ed440702e5
Fruduruk <fruduruk@users.noreply.github.com>
2026-05-06 09:25:06 +0200

    refactored aggregator

--- RoggoStats/roggo_agent/src/core/aggregator.rs
+++ RoggoStats/roggo_agent/src/core/aggregator.rs
@@ -20,47 +20,53 @@ impl Aggregator {
 
     pub fn insert(&mut self, timestamp: u128, raw: String) {
         for event in deserialize(&raw) {
+            self.export_collector_if_finished();
+            self.cancel_if_outdated(event.get_match_guid());
             self.handle_event(timestamp, event);
-            let finished = self
-                .collector
-                .as_ref()
-                .is_some_and(|collector| collector.is_finished());
+        }
+    }
 
-            if finished {
-                if let Some(collector) = self.collector.take() {
-                    self.collected_matches.insert(collector.get_match_guid());
-                    println!("Game {} finished.", collector.get_match_guid());
-                    let stats = collector.export();
-                    println!("Stats: {:#?}", stats);
+    fn export_collector_if_finished(&mut self) {
+        let finished = self
+            .collector
+            .as_ref()
+            .is_some_and(|collector| collector.is_finished());
+
+        if finished {
+            if let Some(collector) = self.collector.take() {
+                self.collected_matches.insert(collector.get_match_guid());
+                println!("Game {} finished.", collector.get_match_guid());
+                let stats = collector.export();
+                // println!("Stats: {:#?}", stats);
+            }
+        }
+    }
+
+    fn cancel_if_outdated(&mut self, match_guid: Option<Uuid>) {
+        if let Some(match_guid) = match_guid {
+            if let Some(collector) = &self.collector {
+                if match_guid != collector.get_match_guid() {
+                    self.collector = None;
                 }
             }
         }
     }
 
     fn handle_event(&mut self, timestamp: u128, event: Event) {
-        if let Some(match_guid) = event.get_match_guid() {
-            let collector = self
-                .collector
-                .get_or_insert_with(|| GameStatCollector::new(match_guid));
+        let Some(match_guid) = event.get_match_guid() else {
+            println!("Discarding event, because it has no match_guid");
+            return;
+        };
 
-            if collector.get_match_guid() == match_guid {
-                if !self.collected_matches.contains(&match_guid) {
-                    collector.insert(timestamp, event);
-                } else {
-                    // println!(
-                    //     "Discarding event, because the agent is done collecting for game: {}",
-                    //     match_guid
-                    // );
-                }
-            } else {
-                println!(
-                    "Discarding event, because the agent is still collecting for a different game: {}, new event match guid: {}",
-                    collector.get_match_guid(),
-                    match_guid
-                );
-            }
-        } else {
-            println!("Discarding event, because it has no match_guid: {event:#?}");
+        if self.collected_matches.contains(&match_guid) {
+            println!("Discarding event, because collection finished for match {match_guid}");
+            return;
         }
+
+        let collector = self
+            .collector
+            .get_or_insert_with(|| GameStatCollector::new(match_guid));
+
+        collector.insert(timestamp, event);
     }
 }
