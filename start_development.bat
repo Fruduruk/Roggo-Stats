f2c94b9d170496fb71ef31b2263f4f108cd8a7a1
Fruduruk <fruduruk@users.noreply.github.com>
2026-05-15 10:24:16 +0200

    new tag

--- start_development.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-start "" code ".\RoggoStats\RoggoDiscordBot"
-start "" code ".\RoggoStats\BallchasingWrapper"
-start "" wt -d ".\RoggoStats"
-start "" wt -d ".\RoggoStats\BallchasingWrapper"
-
-exit
